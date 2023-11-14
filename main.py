a53aee9695cabdc3dfbb34451ac045a660c4ce43
seiya <ilya_shtak@mail.ru>
2023-11-14 21:58:52 +0700

    main done

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import sys
-sys.path.append('internal/api.py')
 import internal.api as api
 
 while True:
-    user_input = input("Введите адрес: ")
+    user_input = input("Введите адрес: \n")
     possible_addresses = api.get_possible_addresses(user_input)
-    
     print("Возможные адреса:")
+    k = 0
     for address in possible_addresses:
-        print(address)
+        k += 1
+        print(f"{k} - {address['value']}")
     
     chosen_address_index = int(input("Выберите номер адреса: ")) - 1
     chosen_address = possible_addresses[chosen_address_index]
