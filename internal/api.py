817a4a493c7060ab612f698665faa7a89890a5cd
seiya <ilya_shtak@mail.ru>
2023-11-15 05:58:24 +0700

    db and settings

--- internal/api.py
+++ internal/api.py
@@ -10,11 +10,11 @@ def get_coords(address):
     coordsx = coords['geo_lat']
     coordsy = coords['geo_lon']
     if coordsx == None and coordsy == None:
-        print('Не получилось определить координаты :(')
+        print('Не получилось определить координаты :( \n'.center(os.get_terminal_size().columns))
     else:
-        print(f'Широта {coordsx}, Долгота {coordsy}')
+        print(f'Широта {coordsx}, Долгота {coordsy} \n'.center(os.get_terminal_size().columns))
 
-def get_possible_addresses(query, preferred_language):
+def get_possible_addresses(query):
     dadata_client = Dadata(os.getenv('API_KEY'), os.getenv('SECRET_KEY'))
-    addresses = dadata_client.suggest(name="address", query=query, language='en')
+    addresses = dadata_client.suggest(name="address", query=query, language='ru')
     return addresses
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import internal.api as api
 import sys
 import time
+import os
+
 
 while True:
     
     user_input_address = input("Введите адрес: \n")
 
-    if user_input_address == 'q':
+    if user_input_address == 'q' or user_input_address == 'й':
         sys.exit()
 
     possible_addresses = api.get_possible_addresses(user_input_address)
-    print("Возможные адреса:")
+    print("Возможные адреса:".center(os.get_terminal_size().columns))
     k = 0
     for address in possible_addresses:
         k += 1
         print(f"{k} - {address['value']}")
     
-    chosen_address_index = int(input("Выберите номер адреса: ")) - 1
+    chosen_address_index = int(input("\n Выберите номер адреса: ")) - 1
     chosen_address = possible_addresses[chosen_address_index]
     
     coordinates = api.get_coords(chosen_address['value'])
-    print("Введите q что бы выйти из программы.")
-    time.sleep(2)
+    print("Введите q что бы выйти из программы.".center(os.get_terminal_size().columns))
+    print("Программа будет автоматически перезапущена через 3 секунды.".center(os.get_terminal_size().columns))
+    time.sleep(3)
