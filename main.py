282c70f456ef643e8be845b69b96beb611bc1076
seiya <ilya_shtak@mail.ru>
2023-11-14 22:23:53 +0700

    keyboard.end done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import internal.api as api
+import sys
+import time
 
 while True:
     user_input = input("Введите адрес: \n")
+    if user_input == 'q':
+        sys.exit()
     possible_addresses = api.get_possible_addresses(user_input)
     print("Возможные адреса:")
     k = 0
@@ -13,3 +17,5 @@ while True:
     chosen_address = possible_addresses[chosen_address_index]
     
     coordinates = api.get_coords(chosen_address['value'])
+    print("Введите q что бы выйти из программы.")
+    time.sleep(2)
